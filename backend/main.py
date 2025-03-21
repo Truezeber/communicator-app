fb5ee2cf857ed671f875e9acd40c05c267e9db6b
Konrad <vobis08gw@gmail.com>
2025-03-21 17:15:56 +0100

    mongo installed

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,26 @@
 from fastapi import FastAPI, HTTPException
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
 
 app = FastAPI()
 
 users = {}
+
+@app.on_event("startup")
+async def startup_db_client():
+    uri = "mongodb+srv://crud-user:BjIQqtjEhUqvCPxw@crud.d14a5.mongodb.net/?retryWrites=true&w=majority&appName=CRUD"
+    app.mongodb_client = MongoClient(uri, server_api=ServerApi('1'))
+    app.mongodb = app.mongodb_client["CRUD"]
+
+    try:
+        app.mongodb_client.admin.command('ping')
+        print("Connected with database")
+    except Exception as e:
+        print(e)
+
+@app.on_event("shutdown")
+def shutdown_db_client():
+    app.mongodb_client.close()
 
 @app.post("/register")
 async def register_user(number: int, password: str, name: str, surname: str ):
