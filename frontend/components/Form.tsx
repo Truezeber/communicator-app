b4fd2c428beedf52cf0194cd58e5b634a0e70e0e
Konrad <vobis08gw@gmail.com>
2025-03-25 16:10:35 +0100

    form w/o logic

--- frontend/components/Form.tsx
+++ frontend/components/Form.tsx
@@ -1,25 +1,99 @@
 "use client";
 
-import { Flex, Tabs } from "@chakra-ui/react";
-import { TiArrowLoopOutline } from "react-icons/ti";
+import {
+  Button,
+  Field,
+  Fieldset,
+  Flex,
+  Input,
+  Stack,
+  Tabs,
+} from "@chakra-ui/react";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+import { CgUser, CgUserAdd } from "react-icons/cg";
+import { useForm, FormProvider } from "react-hook-form";
 
 function Form() {
+  const [signingin, setSigningIn] = useState(false);
+  const [signingup, setSigningUp] = useState(false);
   return (
-    <Tabs.Root defaultValue="members">
-      <Tabs.List>
-        <Tabs.Trigger value="members">
-          <TiArrowLoopOutline />
-          Members
-        </Tabs.Trigger>
-        <Tabs.Trigger value="projects">Projects</Tabs.Trigger>
-        <Tabs.Trigger value="tasks">Settings</Tabs.Trigger>
-      </Tabs.List>
-      <Tabs.Content value="members">Manage your team members</Tabs.Content>
-      <Tabs.Content value="projects">Manage your projects</Tabs.Content>
-      <Tabs.Content value="tasks">
-        Manage your tasks for freelancers
-      </Tabs.Content>
-    </Tabs.Root>
+    <Flex colorPalette="teal">
+      <Tabs.Root defaultValue="members">
+        <Tabs.List>
+          <Tabs.Trigger value="signin">
+            <CgUser />
+            Sign In
+          </Tabs.Trigger>
+          <Tabs.Trigger value="signup">
+            <CgUserAdd />
+            Sign Up
+          </Tabs.Trigger>
+        </Tabs.List>
+        <Tabs.Content value="signin">
+          <Fieldset.Root size="md" maxW="md">
+            <Stack>
+              <Fieldset.Legend>Sign In</Fieldset.Legend>
+              <Fieldset.HelperText>
+                Please tell us who you are
+              </Fieldset.HelperText>
+            </Stack>
+
+            <Fieldset.Content>
+              <Field.Root>
+                <Field.Label>Number</Field.Label>
+                <Input name="lnumber" type="number" />
+              </Field.Root>
+              <Field.Root>
+                <Field.Label>Password</Field.Label>
+                <Input name="lpassword" type="password" />
+              </Field.Root>
+              <Button
+                loading={signingin}
+                onClick={() => setSigningIn(!signingin)}
+              >
+                Sign In
+              </Button>
+            </Fieldset.Content>
+          </Fieldset.Root>
+        </Tabs.Content>
+        <Tabs.Content value="signup">
+          <Fieldset.Root size="md" maxW="md">
+            <Stack>
+              <Fieldset.Legend>Sign Up</Fieldset.Legend>
+              <Fieldset.HelperText>
+                Please tell us who you are
+              </Fieldset.HelperText>
+            </Stack>
+
+            <Fieldset.Content>
+              <Field.Root>
+                <Field.Label>Number</Field.Label>
+                <Input name="number" type="number" />
+              </Field.Root>
+              <Field.Root>
+                <Field.Label>Name</Field.Label>
+                <Input name="name" />
+              </Field.Root>
+              <Field.Root>
+                <Field.Label>Surname</Field.Label>
+                <Input name="surname" />
+              </Field.Root>
+              <Field.Root>
+                <Field.Label>Password</Field.Label>
+                <Input name="password" type="password" />
+              </Field.Root>
+              <Button
+                loading={signingup}
+                onClick={() => setSigningIn(!signingup)}
+              >
+                Sign Up
+              </Button>
+            </Fieldset.Content>
+          </Fieldset.Root>
+        </Tabs.Content>
+      </Tabs.Root>
+    </Flex>
   );
 }
 
