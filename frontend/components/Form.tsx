065b8dbef4abe0b6aaf0beeba88297d7300708e0
Konrad <vobis08gw@gmail.com>
2025-03-26 00:54:54 +0100

    basic signup validation

--- frontend/components/Form.tsx
+++ frontend/components/Form.tsx
@@ -5,6 +5,7 @@ import {
   Field,
   Fieldset,
   Flex,
+  Group,
   Input,
   Stack,
   Tabs,
@@ -20,6 +21,12 @@ function Form() {
 
   const [lnumberError, setlnumberError] = useState("");
   const [lpasswordError, setlpasswordError] = useState("");
+
+  const [numberError, setnumberError] = useState("");
+  const [passwordError, setpasswordError] = useState("");
+  const [rpasswordError, setrpasswordError] = useState("");
+  const [nameError, setnameError] = useState("");
+  const [surnameError, setsurnameError] = useState("");
 
   const router = useRouter();
 
@@ -66,16 +73,47 @@ function Form() {
     } else {
       setlpasswordError("");
     }
+
     console.log(`Form data: ${JSON.stringify(form_signin, null, 2)}`);
   };
 
   const handleSubmit_signup = async () => {
+    if (form_signup.number == null) {
+      setnumberError("Number is required");
+    } else {
+      setnumberError("");
+    }
+
+    if (form_signup.password === "") {
+      setpasswordError("Password is required");
+    } else {
+      setpasswordError("");
+    }
+
+    if (form_signup.password !== form_signup.rpassword) {
+      setrpasswordError("Passwords needs to be the same");
+    } else {
+      setrpasswordError("");
+    }
+
+    if (form_signup.name === "") {
+      setnameError("Name is required");
+    } else {
+      setnameError("");
+    }
+
+    if (form_signup.surname === "") {
+      setsurnameError("Surname is required");
+    } else {
+      setsurnameError("");
+    }
+
     console.log(`Form data: ${JSON.stringify(form_signup, null, 2)}`);
   };
 
   return (
     <Flex colorPalette="teal" suppressHydrationWarning>
-      <Tabs.Root defaultValue="members">
+      <Tabs.Root defaultValue="signin">
         <Tabs.List>
           <Tabs.Trigger value="signin">
             <CgUser />
@@ -130,37 +168,44 @@ function Form() {
             </Stack>
 
             <Fieldset.Content>
-              <Field.Root>
+              <Field.Root invalid={numberError !== ""}>
                 <Field.Label>Number</Field.Label>
                 <Input
                   name="number"
                   type="number"
                   onChange={updateField_signup}
                 />
+                <Field.ErrorText>{numberError}</Field.ErrorText>
               </Field.Root>
-              <Field.Root>
-                <Field.Label>Name</Field.Label>
-                <Input name="name" onChange={updateField_signup} />
-              </Field.Root>
-              <Field.Root>
-                <Field.Label>Surname</Field.Label>
-                <Input name="surname" onChange={updateField_signup} />
-              </Field.Root>
-              <Field.Root>
+              <Group>
+                <Field.Root invalid={nameError !== ""}>
+                  <Field.Label>Name</Field.Label>
+                  <Input name="name" onChange={updateField_signup} />
+                  <Field.ErrorText>{nameError}</Field.ErrorText>
+                </Field.Root>
+                <Field.Root invalid={surnameError !== ""}>
+                  <Field.Label>Surname</Field.Label>
+                  <Input name="surname" onChange={updateField_signup} />
+                  <Field.ErrorText>{surnameError}</Field.ErrorText>
+                </Field.Root>
+              </Group>
+              <Field.Root invalid={passwordError !== ""}>
                 <Field.Label>Password</Field.Label>
                 <Input
                   name="password"
                   type="password"
                   onChange={updateField_signup}
                 />
+                <Field.ErrorText>{passwordError}</Field.ErrorText>
               </Field.Root>
-              <Field.Root>
+              <Field.Root invalid={rpasswordError !== ""}>
                 <Field.Label>Repeat password</Field.Label>
                 <Input
                   name="rpassword"
                   type="password"
                   onChange={updateField_signup}
                 />
+                <Field.ErrorText>{rpasswordError}</Field.ErrorText>
               </Field.Root>
               <Button loading={spinning} onClick={handleSubmit_signup}>
                 Sign Up
